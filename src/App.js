1a28ce994733301de281581e4bac3b42b9241613
최명범 <audqja3400@naver.com>
2019-04-20 14:54:11 +0900

    커밋

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import {Header} from "./components/Header";
+import {Player} from "./components/Player";
 
-class App extends Component {
+class App extends React.Component{
+  state = {
+    players : [
+      {name : "LDK" , id:1},
+      {name : "HONG" , id:2},
+      {name : "KIM" , id:3},
+      {name : "PARK" , id:4}
+    ]
+  }
+
+  handleRemovePlayer = (id) => {
+    this.setState(prevState => ({
+      players : prevState.players.filter(player => player.id !== id)
+    }))
+  }
+
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+    return(
+      <div className="scoreboard">
+        <Header title="My Scoreboard" totalPllayers ={11} />
+
+        {
+          this.state.players.map(player => <Player name={player.name}
+                                                   removePlayer={this.handleRemovePlayer}
+                                                   id = {player.id}
+                                                   key={player.id}/>)
+        }
       </div>
     );
   }
